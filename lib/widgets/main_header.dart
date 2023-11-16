008cb52ceae0571e3843c0a12916b789f661b741
William Hatzar <williamhatzar@Williams-MacBook-Pro.local>
2023-11-16 00:26:55 +0000

    Added in theme changer functionality

--- lib/widgets/main_header.dart
+++ lib/widgets/main_header.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/material.dart';
 
 class Header extends StatefulWidget {
-  const Header({Key? key}) : super(key: key);
+  final List<String> items;
+  final int currentIndex;
+  final Function() onPressedCallback;
+  Color getTitleColour() {
+    if (items[currentIndex] == "Theme 1") {
+      return Colors.white;
+    } else if (items[currentIndex] == "Theme 2") {
+      return const Color(0xff34362C);
+    } else {
+      return const Color(0xffFEE844);
+    }
+  }
+
+  const Header({
+    Key? key,
+    required this.items,
+    required this.currentIndex,
+    required this.onPressedCallback,
+  }) : super(key: key);
 
   @override
   State<Header> createState() => _HeaderState();
 }
 
 class _HeaderState extends State<Header> {
+  String getTheme() {
+    return widget.items[widget.currentIndex];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Text("calc",
+         Text("calc",
             style: TextStyle(
                 fontFamily: "League Spartan",
                 fontSize: 30.0,
                 fontWeight: FontWeight.bold,
-                color: Colors.white
-            )
+                color: widget.getTitleColour())),
+        const SizedBox(width: 220.0),
+        GestureDetector(
+          key: UniqueKey(), // Add a unique key to force the GestureDetector to rebuild
+          onTap: () {
+            widget.onPressedCallback();
+          },
+          child: Text(
+              "${getTheme()}",
+              style: TextStyle(
+                  fontFamily: "League Spartan",
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: widget.getTitleColour())),
         )
       ],
     );
